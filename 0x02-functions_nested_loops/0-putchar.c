1eb4f4292d97b526333d901cedd607ca6350dc56
chisom chinaza <chisomchinazam@gmail.com>
2022-12-28 21:53:10 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,16 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - print _putchar
- *  Return: 0
+ * main - prints _putchar as a message
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
-
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+
+printf("_putchar\n");
+
 return (0);
 }
